c07fa846e420f24c7ded842186d9c52cab77289f
ashleyjr <ashley181291@gmail.com>
2021-10-11 19:35:30 +0100

    uart working

--- throwie/software/src/main.c
+++ throwie/software/src/main.c
@@ -15,20 +15,21 @@ SBIT(LED,  SFR_P1, 1);
 // Prototypes
 //-----------------------------------------------------------------------------
 
-void  sleep(void);
-void  setup(void);
+void uartTx(U8 tx);
+void sleep(void);
 
 //-----------------------------------------------------------------------------
 // Main Routine
 //-----------------------------------------------------------------------------
 
-void main (void){    
-   sleep();
+void main (void){     
    while(1){
-      LED = 0;
       sleep();
       LED = 1;
+      uartTx('A');
       sleep();
+      LED = 0;
+      uartTx('A');
    };
 }
  
@@ -36,12 +37,21 @@ void main (void){
 // Interrupt Vectors
 //-----------------------------------------------------------------------------
 
-INTERRUPT (TIMER1_ISR, TIMER1_IRQn){
+INTERRUPT (TIMER0_ISR, TIMER0_IRQn){
    IE = 0;
+}
+//-----------------------------------------------------------------------------
+// UART
+//-----------------------------------------------------------------------------
+
+void uartTx(U8 tx){
+   SCON0_TI = 0;
+   SBUF0 = tx;
+   while(!SCON0_TI); 
 }
 
 //-----------------------------------------------------------------------------
-// Setup micro
+// Setup/sleep
 //-----------------------------------------------------------------------------
 
 void sleep(void){
@@ -59,15 +69,15 @@ void sleep(void){
 
    // Enabled T1 interrupt
    IE   = IE_EA__ENABLED | 
-		    IE_ET1__ENABLED;
+		    IE_ET0__ENABLED;
    
 
-   // Configure T1
-   CKCON  = CKCON_T1M__PRESCALE|
+   // Configure T0
+   CKCON  = CKCON_T0M__PRESCALE|
             CKCON_SCA__SYSCLK_DIV_48;  
-	TMOD   = TMOD_T1M__MODE1;	 
-   TL1    = 0xF0;
-   TH1    = 0xFF;
+	TMOD   = TMOD_T0M__MODE1;	 
+   TL0    = 0xFA;
+   TH0    = 0xFF;
    
 
    // Main clock is the Low Frequency Clock 
@@ -79,8 +89,8 @@ void sleep(void){
    CLKSEL = CLKSEL_CLKSL__LFOSC|
             CLKSEL_CLKDIV__SYSCLK_DIV_128 ;
    
-   // Start T1
-   TCON   = TCON_TR1__RUN;     
+   // Start T0
+   TCON   = TCON_TR0__RUN;     
 
 
    // Go to sleep
@@ -93,90 +103,25 @@ void sleep(void){
 
    // Clock
 	CLKSEL   = CLKSEL_CLKSL__HFOSC |			     
-              CLKSEL_CLKDIV__SYSCLK_DIV_1;
-
- 
-                                                // Enable IOs
-   P1SKIP   = P1SKIP_B1__SKIPPED;
-   P1MDOUT  = P1MDOUT_B1__PUSH_PULL;
-   XBR2     = XBR2_WEAKPUD__PULL_UPS_DISABLED | 
-              XBR2_XBARE__ENABLED;					  
-}
-
-//-----------------------------------------------------------------------------
-// Setup micro
-//-----------------------------------------------------------------------------
-
-void setup(void){
-   // Disabled watchdog
-   WDTCN    = 0xDE;
-   WDTCN    = 0xAD;
-   // Clock
-	CLKSEL   = CLKSEL_CLKSL__HFOSC 	      |     // Use 24.5MHz interal clock
-			     CLKSEL_CLKDIV__SYSCLK_DIV_1;      // Do not divide     
-   // Setup XBAR       
+              CLKSEL_CLKDIV__SYSCLK_DIV_1; 
+   
+   // Enable IOs
    P0MDOUT  = P0MDOUT_B4__PUSH_PULL;
-   P1SKIP   = P1SKIP_B0__SKIPPED|
-              P1SKIP_B1__SKIPPED;
-   P1MDOUT  = P1MDOUT_B0__PUSH_PULL|            // LED            
-              P1MDOUT_B1__PUSH_PULL;            // LED 
-   XBR0     = XBR0_SMB0E__ENABLED| 
-              XBR0_URT0E__ENABLED;              // Route out UART P0.4 
+   P1SKIP   = P1SKIP_B1__SKIPPED; 
+   P1MDOUT  = P1MDOUT_B1__PUSH_PULL;
+   XBR0     = XBR0_URT0E__ENABLED;
    XBR2     = XBR2_WEAKPUD__PULL_UPS_DISABLED | 
-              XBR2_XBARE__ENABLED;					  
-   // Timer control
-	CKCON    = CKCON_T0M__SYSCLK|
-              CKCON_SCA__SYSCLK_DIV_12;  
-   // SMBus
-   SMB0CF   = SMB0CF_INH__SLAVE_DISABLED|
-              SMB0CF_SMBCS__TIMER0|
-              SMB0CF_SMBFTE__FREE_TO_ENABLED| 
-              SMB0CF_ENSMB__ENABLED; 
-   // I2C clock on timer 0
-   // BAUD gen 115200 on timer 1
-	CKCON    |= CKCON_T1M__SYSCLK;
-	TMOD     |= TMOD_T0M__MODE2 |
-               TMOD_T1M__MODE2;
-	TCON     |= TCON_TR0__RUN |
-               TCON_TR1__RUN; 
-   TH0      = 0xFE;  // I2C SCL - 333KHz (above 100KHz max for drivers datasheet)
-   TL0      = 0x00;
+              XBR2_XBARE__ENABLED;
+
+   // Setup Timers
+   CKCON    = CKCON_T1M__SYSCLK;  
+	TMOD     = TMOD_T1M__MODE2;
+	TCON     = TCON_TR1__RUN; 
    TH1      = 0x96;  // Magic values from datasheet for 115200
 	TL1      = 0x96;
-   // UART
-	SCON0    |= SCON0_REN__RECEIVE_ENABLED; 
-   // Timer 2
-	TMR2CN   = TMR2CN_TR2__RUN;   // ~10KHz 
-   TMR2L    = 0x00;
-   TMR2H    = 0xFF;
-   TMR2RLL  = 0x00;
-   TMR2RLH  = 0xFF;
-   // Timer 3
-	TMR3CN   = TMR3CN_TR3__RUN;   // ~44 KHz
-   TMR3L    = 0x50;
-   TMR3H    = 0xFF;
-   TMR3RLL  = 0x50;
-   TMR3RLH  = 0xFF;
-   // ADC
-   ADC0MX   = ADC0MX_ADC0MX__ADC0P3;
-   ADC0CF   = ADC0CF_ADGN__GAIN_1 |       // ADC gain set to 1
-              ADC0CF_ADTM__TRACK_NORMAL;  // Immediate covert
-   REF0CN   = REF0CN_REFSL__VDD_PIN;      // Ref to VDD
-   ADC0CN0 &= ~ADC0CN0_ADCM__FMASK;
-   ADC0CN0 |= ADC0CN0_ADEN__ENABLED |
-              ADC0CN0_ADCM__ADBUSY; 
+  
    // Interrupts
-	IE   = IE_EA__ENABLED | 
-		    IE_ET0__ENABLED|
-          IE_ET1__ENABLED|
-          IE_ET2__ENABLED;
-   EIE1 = EIE1_ESMB0__ENABLED|
-          EIE1_ET3__ENABLED;
-   IP   = IP_PT2__HIGH;    
+   IE = IE_EA__ENABLED;
 }
-
-
-
-
 
 
