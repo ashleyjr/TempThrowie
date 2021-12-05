f73117ada981efa676313b56ecaf8376488b1ca4
ashleyjr <ashley181291@gmail.com>
2021-12-05 13:14:45 +0000

    started sample

--- receiver/software/mcu/src/main.c
+++ receiver/software/mcu/src/main.c
@@ -9,27 +9,113 @@
 // Defines
 //-----------------------------------------------------------------------------
 
+#define DBG_SAMPLE
+
+SBIT(RX,       SFR_P0,  7);  
+#ifdef DBG_SAMPLE
+SBIT(SAMPLE,   SFR_P1,  4);  
+#endif
+
 //-----------------------------------------------------------------------------
 // Global Variables
 //-----------------------------------------------------------------------------
 
+unsigned char sample_pin;
+unsigned char pattern[8];
+
 //-----------------------------------------------------------------------------
 // Prototypes
 //-----------------------------------------------------------------------------
+
+void uartTx(U8 tx);
+void setup(void);
 
 //-----------------------------------------------------------------------------
 // Main Routine
 //-----------------------------------------------------------------------------
 
 void main (void){       
-   for(;;); 
+   setup();
+   sample_pin=0;
+   SAMPLE=0;
+   for(;;){
+//      uartTx('t');
+   }
 }
  
 //-----------------------------------------------------------------------------
 // Interrupt Vectors
 //-----------------------------------------------------------------------------
 
+INTERRUPT (TIMER2_ISR, TIMER2_IRQn){           
+   
+   #ifdef DBG_SAMPLE
+   // Toggle the sample pin
+   sample_pin++;
+   sample_pin %= 2;
+   SAMPLE = sample_pin; 
+   #endif
+
+   
+
+   TMR2CN &= ~TMR2CN_TF2H__SET;
+} 
+
 //-----------------------------------------------------------------------------
-// Setup/sleep
+// UART
 //-----------------------------------------------------------------------------
+
+void uartTx(U8 tx){
+   SCON0_TI = 0;
+   SBUF0 = tx;
+   while(!SCON0_TI); 
+}
+
+//-----------------------------------------------------------------------------
+// Setup
+//-----------------------------------------------------------------------------
+
+void setup(void){
+
+   // Disabled watchdog
+   WDTCN    = 0xDE;
+   WDTCN    = 0xAD;
+
+	CLKSEL   = CLKSEL_CLKSL__HFOSC |			     
+              CLKSEL_CLKDIV__SYSCLK_DIV_1; 
+   
+   // Enable IOs 
+   P0MDIN   = P0MDIN_B7__DIGITAL; 
+   #ifdef DBG_SAMPLE
+   P1MDOUT  = P1MDOUT_B4__PUSH_PULL; 
+   #endif
+
+   XBR0     = XBR0_URT0E__ENABLED; 
+   XBR2     = XBR2_WEAKPUD__PULL_UPS_DISABLED | 
+              XBR2_XBARE__ENABLED;
+   
+   // Setup Timers
+   CKCON    = CKCON_T1M__SYSCLK| 
+	           CKCON_T2ML__SYSCLK|
+              CKCON_T2MH__SYSCLK;
+
+   // Tiemr 1: UART 
+   TMOD     = TMOD_T1M__MODE2;
+	TCON     = TCON_TR1__RUN; 
+   TH1      = 0x96;           // Magic values from datasheet for 115200
+	TL1      = 0x96; 
+
+   // Timer 2: Counter 2.68KHz
+   // TODO: should be 2KHz
+	TMR2CN   = TMR2CN_TR2__RUN; 
+   TMR2L    = 0x0F;
+   TMR2H    = 0xEE;
+   TMR2RLL  = 0x0F;
+   TMR2RLH  = 0xEE;
+   
+   // Interrupts
+   IE = IE_EA__ENABLED | 
+        IE_ET2__ENABLED;
+}
+
 
