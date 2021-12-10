abe6b7cadc3477c733f43c252ad1bb7f2d8a5d03
ashleyjr <ashley181291@gmail.com>
2021-12-10 23:40:30 +0000

    pd

--- modelling/receiver_pll/receiver_pll.py
+++ modelling/receiver_pll/receiver_pll.py
@@ -33,10 +33,26 @@ class SquareWave(DiscreteSim):
         self.cycle  += self.timestep_s
         self.cycle  %= self.period_s
 
+class PhaseDet(DiscreteSim):
+    def __init__(   self,
+                    timestep_s  = 1e-6):
+        DiscreteSim.__init__(self, timestep_s)
+        self.a = 0
+        self.b = 0
+
+    def setInputA(self, a):
+        self.a = a
+
+    def setInputB(self, b):
+        self.b = b
+
+    def getOutput(self):
+        return self.a - self.b
+
 class Vco(SquareWave):
     def __init__(   self,
                     timestep_s  = 1e-6,
-                    min_freq_hz = 1e3,
+                    min_freq_hz = 2e3,
                     max_freq_hz = 1e6):
         SquareWave.__init__(self, timestep_s)
         self.__max_period_s = 1 / float(min_freq_hz)
@@ -84,10 +100,15 @@ class Scope(DiscreteSim):
 def main(argv):
     wave = SquareWave()
     vco = Vco()
-    scope = Scope(channels=2)
+    pd = PhaseDet()
+    scope = Scope(channels=3)
     for i in range(10000):
+        pd.setInputA(wave.getOutput())
+        pd.setInputB(vco.getOutput())
         scope.measure(wave.getOutput(),0)
         scope.measure(vco.getOutput(),1)
+        scope.measure(pd.getOutput(),2)
+        pd.tick()
         scope.tick()
         wave.tick()
         vco.tick()
