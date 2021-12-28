7682132c51ec138ffe048af7df0cb7ae12ae0976
ashleyjr <ashley181291@gmail.com>
2021-12-28 19:56:37 +0000

    add model to C receiver build

---
+++ modelling/receiver_pll/receiver_pll.c
@@ -0,0 +1,4 @@
+
+int receiver_pll(int i) {
+      return i;
+}
--- receiver/software/mcu/src/main.c
+++ receiver/software/mcu/src/main.c
@@ -10,22 +10,6 @@
 //-----------------------------------------------------------------------------
 
 #define DBG_SAMPLE
-#define BUFFER_DEPTH_BYTES  5
-#define BUFFER_DEPTH_BITS  (8*BUFFER_DEPTH_BYTES)
-
-#define SM_CODE0   0
-#define SM_CODE1   1
-#define SM_CODE2   2
-#define SM_CODE3   3
-#define SM_CODE4   4
-#define SM_CODE5   5
-#define SM_CODE6   6
-#define SM_CODE7   7
-#define SM___ZERO  8
-#define SM_N_ZERO  9
-#define SM___ONE   10
-#define SM_N_ONE   11
-
 
 SBIT(RX,       SFR_P0,  7);  
 #ifdef DBG_SAMPLE
@@ -36,15 +20,13 @@ SBIT(SAMPLE,   SFR_P1,  4);
 // Global Variables
 //-----------------------------------------------------------------------------
 
-volatile U8 found;
-volatile U8 buffer_head;
-volatile U8 buffer_tail;
-volatile U8 buffer[BUFFER_DEPTH_BYTES];
+volatile U8 sample;
 
 //-----------------------------------------------------------------------------
 // Prototypes
 //-----------------------------------------------------------------------------
 
+int receiver_pll(int i);
 void uartTx(U8 tx);
 void setup(void);
 
@@ -53,10 +35,8 @@ void setup(void);
 //-----------------------------------------------------------------------------
 
 void main (void){        
-   buffer_head = 0;
-   buffer_tail = 0;  
+   sample=0;
    setup();
-   SAMPLE      = 0;
    for(;;);
 }
  
@@ -65,108 +45,26 @@ void main (void){
 //-----------------------------------------------------------------------------
 
 INTERRUPT (TIMER2_ISR, TIMER2_IRQn){            
-   U8 i;
-   U8 ptr;
-   U8 by;
-   U8 bt;
-   U8 state;
-
+   // Disable all interrupts
    IE = 0;   
 
    #ifdef DBG_SAMPLE
-   // Toggle the sample pin 
-   SAMPLE = 1; 
+   // Start the sample 
+   //SAMPLE = 1; 
    #endif
 
-   // Access bytes/bits
-   by = buffer_head >> 3;
-   bt = buffer_head & 0x07;
-  
-   // Masking
-   if(RX){
-      buffer[by] |=  (1 << bt);
-   }else{
-      buffer[by] &= ~(1 << bt);
-   }
+   // Call the lock function
+   SAMPLE = receiver_pll(RX);
 
-   // Update pointer
-   buffer_head++;
-   buffer_head %= BUFFER_DEPTH_BITS;
-
-   found = 1;
-   state = SM_CODE0;
-   ptr   = buffer_head; 
-   do{
-      by = ptr >> 3;
-      bt = ptr & 0x07;
-      bt = 0x01 & (buffer[by] >> bt);
-      switch(state){
-         case SM_CODE0:    state = SM_CODE1; 
-                           break;
-         case SM_CODE1:    if(1 == bt) found = 0;
-                           state = SM_CODE2;
-                           break;
-         case SM_CODE2:    state = SM_CODE3; 
-                           break;
-         case SM_CODE3:    if(0 == bt) found = 0;
-                           state = SM_CODE4;
-                           break;
-         case SM_CODE4:    state = SM_CODE5; 
-                           break;
-         case SM_CODE5:    if(0 == bt) found = 0;
-                           state = SM_CODE6;
-                           break;
-         case SM_CODE6:    state = SM_CODE7; 
-                           break;
-         case SM_CODE7:    if(1 == bt) found = 0;
-                           state = SM_N_ONE;
-                           break;
-         case SM_N_ONE:    state = SM___ONE;
-                           break; 
-         case SM___ONE:    if(0 == bt) found = 0;
-                           state = SM_N_ZERO;
-                           break;
-         case SM_N_ZERO:   state = SM___ZERO;
-                           break;
-         case SM___ZERO:   if(1 == bt) found = 0;
-                           state = SM_N_ONE;
-                           break; 
-      }
-      
-      if(ptr == 0){
-         ptr = BUFFER_DEPTH_BITS-1;
-      }else{
-         ptr--;
-      } 
-   }while(ptr != buffer_head); 
-
-   if(found){
-      ptr = buffer_head;
-      do{
-         by = ptr >> 3;
-         bt = ptr & 0x07;
-         bt = 0x01 & (buffer[by] >> bt); 
-         uartTx(bt + '0');
-         if(ptr == 0){
-            ptr = BUFFER_DEPTH_BITS-1;
-         }else{
-            ptr--;
-         } 
-      }while(ptr != buffer_head);  
-      uartTx('\n');
-      uartTx('\r');
-
-      // Clear the buffer
-      for(i=0;i<BUFFER_DEPTH_BYTES;i++){
-         buffer[i] = 0x00;
-      }
-
-   }
-   // Buffer read to access 
-   SAMPLE = 0; 
+   // Enable the interrupts
    TMR2CN &= ~TMR2CN_TF2H__SET;
    IE = IE_EA__ENABLED | 
         IE_ET2__ENABLED; 
+   
+   #ifdef DBG_SAMPLE
+   // End the sample 
+   //SAMPLE = 0; 
+   #endif 
 } 
 
 //-----------------------------------------------------------------------------
@@ -219,13 +117,14 @@ void setup(void){
    TH1      = 0x96;           // Magic values from datasheet for 115200
 	TL1      = 0x96; 
 
-   // Timer 2: Counter 2.68KHz
-   // TODO: should be 2KHz
+   // Timer 2: 
+   //    - Runs as fast as possible
+   //    - 
 	TMR2CN   = TMR2CN_TR2__RUN; 
-   TMR2L    = 0x0F;
-   TMR2H    = 0xEE;
-   TMR2RLL  = 0x0F;
-   TMR2RLH  = 0xEE;
+   TMR2L    = 0xD0;
+   TMR2H    = 0xFF;
+   TMR2RLL  = 0xD0;
+   TMR2RLH  = 0xFF;
    
    // Interrupts
    IE = IE_EA__ENABLED | 
