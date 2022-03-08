a50d337e214acbaf300ba8f5f48cdc651ff7c82a
Ashley J. Robinson <ashley181291@gmail.com>
2022-03-08 23:14:38 +0000

    add cuts for jig base

--- throwie/prog_jig/RevA/jig_base.py
+++ throwie/prog_jig/RevA/jig_base.py
@@ -6,18 +6,30 @@ from FreeCAD import Base
 # exec(open(<path_to_file>,'r').read())
 
 # Parameters
-base_length     = 70
-base_width      = 50
-base_depth      = 20
+base_length     = 38
+base_width      = 32
+base_depth      = 15
 
-alcove_length   = 50
-alcove_width    = 30
+alcove_length   = 32
+alcove_width    = 19
 alcove_depth    = 10
+alcove_offset   = 3
 
-shelf_width     = 2
-shelf_depth     = 5
+ant_width   = 2
+ant_length  = 8
 
-stantion_height   = 40
+ldo_width  = 13
+ldo_length = 11
+
+pwr_width  = 16
+pwr_length = 12
+pwr_offset  = 13
+
+fet_width = 16
+fet_length = 10
+fet_offset = 1
+
+stantion_height   = 25
 stantion_diameter = 5
 
 # Name
@@ -31,31 +43,55 @@ App.setActiveDocument(name)
 App.ActiveDocument=App.getDocument(name)
 Gui.ActiveDocument=Gui.getDocument(name)
 
-# Cut shelf out the alcove, cut alcove out of base
+# The major base
 base=Part.makeBox(base_length,base_width,base_depth)
 
-shelf=Part.makeBox(
-    alcove_length-shelf_width,
-    alcove_width-(2*shelf_width),
-    shelf_depth)
-
-x = 0
-y = (base_width - alcove_width + (2*shelf_width)) / 2
-z = (base_depth - alcove_depth)
-shelf.translate(Base.Vector(x,y,z))
-
+# The alcove cut to fit the PCB
 alcove=Part.makeBox(
     alcove_length,
     alcove_width,
-    alcove_depth-shelf_depth)
+    alcove_depth)
 
-x = 0
+x = alcove_offset
 y = (base_width - alcove_width) / 2
-z = (base_depth - alcove_depth + shelf_depth)
+z = (base_depth - alcove_depth)
 alcove.translate(Base.Vector(x,y,z))
 
-base=base.cut(shelf)
+# Antenna cut
+ant=Part.makeBox(alcove_offset+ant_length,ant_width,base_depth)
+x = 0
+y = (base_width - alcove_width) / 2
+z = 0
+ant.translate(Base.Vector(x,y,z))
+
+# LDO cut
+ldo = Part.makeBox(ldo_length,ldo_width,base_depth)
+x = alcove_offset
+y = (alcove_width - ldo_width) + ((base_width - alcove_width) / 2)
+z =0
+ldo.translate(Base.Vector(x,y,z))
+
+# PWR cut
+pwr = Part.makeBox(pwr_length,pwr_width,base_depth)
+x = alcove_offset + pwr_offset
+y = (alcove_width - pwr_width) + ((base_width - alcove_width) / 2)
+z =0
+pwr.translate(Base.Vector(x,y,z))
+
+# PWR cut
+fet = Part.makeBox(fet_length,fet_width,base_depth)
+x = alcove_offset + alcove_length - fet_length
+y = (fet_offset) + ((base_width - alcove_width) / 2)
+z =0
+fet.translate(Base.Vector(x,y,z))
+
+
+
 base=base.cut(alcove)
+base=base.cut(ant)
+base=base.cut(ldo)
+base=base.cut(pwr)
+base=base.cut(fet)
 Part.show(base)
 
 stantion0=Part.makeCylinder(
